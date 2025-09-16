d96f8ff5476918d6c948990cbb28e67dbaac9f94
Umair505 <moinuli359@gmail.com>
2025-09-16 10:41:48 +0600

    Even Odds sequence problem solved

---
+++ JuniorTrainingSheet/A_Even_Odds.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,k;
+    cin>>n>>k;
+   
+    long long int oddCount = (n+1) / 2;
+    if(k <= oddCount)
+    {
+        cout<<2*k - 1<<endl;
+    }
+    else{
+        cout<<2*(k-oddCount)<<endl;
+    }
+
+    return 0;
+}
